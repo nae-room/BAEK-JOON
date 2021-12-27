63a2eefb7842439689e1555fbbb59f0cef2812a3
김 내루미 <12191726@inha.edu>
2021-12-27 18:41:39 +0900

    2021.12.27

--- 1000 to 9999/1003.cpp
+++ 1000 to 9999/1003.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int n) {
+    if (n == 0) {
+        printf("0");
+        return 0;
+    } else if (n == 1) {
+        printf("1");
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
 int main(){
-    cout << "바탕";
-    return 0;
+
 }
