cba35fcf53134bd4cd79c88a950ed373dd91bcad
김 내루미 <12191726@inha.edu>
2021-12-28 17:44:28 +0900

    2021.12.28

---
+++ 10000 to 19999/10951.cpp
@@ -0,0 +1,16 @@
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+
+    while (cin >> A >> B) {
+    cout << A + B << endl;
+    }
+
+    return 0;
+}
+
+// while : () 안이 참이면 실행
